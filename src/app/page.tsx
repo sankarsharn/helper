56d8fb7bbfa9a7535e960c05a48eea71149152b0
sankarsharn <sankarsharnrastogi@gmail.com>
2025-03-25 17:45:23 +0530

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import InfiniteScroll from './components/InfiniteScroll'
 import Demo from './components/Demo'
-import Globe  from './components/Globe'
+// import Globe  from './components/Globe'
 import Card from './components/Card'
 const page = () => {
   return (
@@ -10,7 +10,7 @@ const page = () => {
     <Demo />
     <InfiniteScroll />
     <Card />
-    <Globe />
+    {/* <Globe /> */}
 
     </>
   )
